46990f66a970e00deaeb078c7243cfb8ff383893
Sherri Li <32345806+shli17@users.noreply.github.com>
2018-09-19 15:52:58 -0700

    Update example1.py

--- demo/example1.py
+++ demo/example1.py
@@ -5,25 +5,25 @@ from selenium.webdriver.common.keys import Keys
 
 
 # Create a ChromeDriver instance
-self.driver = webdriver.Chrome()
+driver = webdriver.Chrome()
 
 # Wait up to 30 seconds for the browser to load
-self.driver.implicitly_wait(30)
+driver.implicitly_wait(30)
 
 # Open google home page
-self.driver.get("https://www.google.com")
+driver.get("https://www.google.com")
 
 # Locate the search bar
-self.driver.find_element_by_name("q").clear()
+driver.find_element_by_name("q").clear()
 
 # Search for 'cheese'
-self.driver.find_element_by_name("q").send_keys("cheese")
-self.driver.find_element_by_name("q").send_keys(Keys.ENTER)
+driver.find_element_by_name("q").send_keys("cheese")
+driver.find_element_by_name("q").send_keys(Keys.ENTER)
 
 # Click the 'images' tab
-self.driver.find_element_by_link_text("Images").click()
+driver.find_element_by_link_text("Images").click()
 
 # Take a screenshot
-self.driver.save_screenshot('exampleCheese.png')
+driver.save_screenshot('exampleCheese.png')
 
-self.driver.quit()
+driver.quit()
