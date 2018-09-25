4800ced0efafdd73cb42550dc278bcc390847188
Sherri Li <32345806+shli17@users.noreply.github.com>
2018-09-25 14:00:04 -0700

    Update LOGIN_TEST.py

--- Login Tests/LOGIN_TEST.py
+++ Login Tests/LOGIN_TEST.py
@@ -12,20 +12,22 @@ Author      : Sherri Li
 """
 print(intro)
 
-import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import unittest
 import base64
 import os
 import json
 import datetime
 import time
+import timeit
+import logging
 import subprocess
 #import sys
 #sys.path.append("..")
-import lastpass-login
-import log-test
+import get_credentials
+import write_log
 import site-functions
 
 # This test generates a log file whose file name is dynamically generated, based on system time.
@@ -33,7 +35,7 @@ import site-functions
 timeOfTest = str(datetime.datetime.now()).replace(' ', '')[0:16]
 logging.basicConfig(filename=timeOfTest + '_LoginTest.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
-class login_test(unittest.TestCase):
+class LoginTest(unittest.TestCase):
     appName = 'ches-dev' # can change to whatever group of apps you wish to test
     browser = None
     browserType = None
@@ -45,9 +47,11 @@ class login_test(unittest.TestCase):
     while (browserType!='chrome' and browserType!='firefox'):
         browserType = input("Enter the browser you wish to test on (chrome or firefox): ")
 
-    # init is a constructor that runs for every test module below
+        
+# SETUP FUNCTIONS--------------------------------------------------
+    # Init is a constructor that creates instances of the LoginTest object.
     def __init__(self, *args, **kwargs):
-        super(login_test, self).__init__(*args, **kwargs)
+        super(LoginTest, self).__init__(*args, **kwargs)
         if self.browser is None:
             self.setupBrowser(self.browserType)
         if self.pwsites is None:
@@ -71,15 +75,20 @@ class login_test(unittest.TestCase):
         mm = None
         dd = None
         yr = None
+        if self.timeStart is None:
+            self.timeStart = timeit.default_timer()
 
     def tearDown(self):
-        pass
+        if self.timeStart is not None:
+            timeElapsed = timeit.default_timer() - self.timeStart
+            write_log.logSummary(self.browserType, timeElapsed)
 
+# We assume that the tester has access to the credential files / shared folder in LastPass.
+# This file will contain the credentials, in JSON format, to each app we are testing.
 
-#CASE ONE: all credentials are in one LastPass file
     def setupPwSites(self):
         # Grab all credentials from the json file in lpass
-        creds = login_lp.get_all(self.appName+'-pw-creds.json')
+        creds = get_credentials.get_all("name-of-lastpass-credential-file-here")
         # turn STRING into JSON:
         jcreds = json.loads(creds)
         self.pwsites = {}
@@ -115,9 +124,7 @@ class login_test(unittest.TestCase):
 
 
     def setupBdSites(self):
-        # CALL LOGIN_LP HERE INSTEAD
-        # SAVE THE RETURN STRING TO A PYTHON LIST/DICT
-        creds = login_lp.get_all(self.appName+'-bd-creds.json')
+        creds = get_credentials.get_all("name-of-lastpass-credential-file-here")
         jcreds = json.loads(creds)
         self.bdsites = {}
         if self.types=='dev-new':
@@ -156,9 +163,6 @@ class login_test(unittest.TestCase):
                 self.bdsites[siteData['siteName']] = site
 
 
-# TODO: CASE TWO: each site has its own credentials file in LastPass
-
-
     def setupBrowser(self, browserName):
         if browserName=='firefox':
             try:
@@ -175,8 +179,9 @@ class login_test(unittest.TestCase):
             except:
                 print("Unable to load chrome")
 
-    # Note: only functions whose names start with "test" get automatically run by the call at the bottom 'unittest.main()'
-
+                
+# Note: Functions whose names start with "test" are automatically run by the call at the bottom 'unittest.main()'
+# TEST FUNCTIONS--------------------------------------------------
     def test_name_of_site1(self):
         self.password_login_test("name_of_site","name_of_username_field","name_of_password_field","name_of_submit_button")
 
@@ -184,13 +189,16 @@ class login_test(unittest.TestCase):
         self.birthday_login_test("name_of_site","name_of_username_field","name_of_month_field","name_of_day_field","name_of_year_field","name_of_submit_button")
     
     # ... you can add more tests for the sites you want here
-    
+  
 
+# Note: These helper functions are not called by the unittest directly, but instead called by the test functions above.
+# HELPER FUNCTIONS--------------------------------------------------
     # Generic login test to a site requiring USERNAME and PASSWORD credentials
-    def password_login_test(self,siteName,usernameBox,passwordBox,loginButtonName):
+    # text: string you use to verify that the login succeeded (choose text that appears after login but not before login)
+    # siteName: file name of the account in LastPass
+    def password_login_test(self,text,siteName,usernameBox,passwordBox):
         assert(self.browser is not None)
         assert(self.pwsites is not None)
-        text = "Work-study Information"
         browser = self.browser
         
         # Make sure the site credentials exist in your LastPass
@@ -211,12 +219,10 @@ class login_test(unittest.TestCase):
         # Open drivers after ensuring status code is acceptable.
         try:
             browser.get(site['dev_url'])
-            # Call function from log-test.py file to write message to log file
-            log-test.logStart("name_of_site_you_are_testing_here")
+            # Call function from write_log.py file to write message to log file
+            write_log.logStart("name_of_site_you_are_testing_here")
             print("Testing " + site['dev_url'])
 
-            self.username = site['id']
-            self.password = site['password']
             username_txt = browser.find_element_by_name(usernameBox)
             password_txt = browser.find_element_by_name(passwordBox)
             #submit_txt = browser.find_element_by_xpath("(//input[@name='act'])[2]")
@@ -226,23 +232,26 @@ class login_test(unittest.TestCase):
             #assert(submit_txt is not None)
 
             username_txt.clear()
-            username_txt.send_keys(self.username)
+            username_txt.send_keys(site['id'])
             password_txt.clear()
-            password_txt.send_keys(self.password)
-            #submit_txt.click()
-            browser.find_element_by_name(passwordBox).send_keys(Keys.ENTER) # submit button sometimes not visible
+            password_txt.send_keys(site['password'])
+            # Simulating an "enter" keypress is more efficient than searching for the submit button.
+            password_txt.send_keys(Keys.ENTER)
 
+            browser.implicitly_wait(30)
+            
             if text not in browser.page_source:
                 print("ERROR: " + text + " not in " + siteName + " source after successful login\n")
             assert(text in browser.page_source)
 
-            logout_txt = browser.find_element_by_xpath("//area") # parametrize to logoutButtonName
+            # Sometimes the logout button has no id, or name, so you can only find it by xpath.
+            logout_txt = browser.find_element_by_xpath(logoutButton)
             if logout_txt==None:
                 print(siteName + " has no logout button")
-            assert(logout_txt is not None)
-            logout_txt.click()
+            else:
+                logout_txt.click()
             
-            log-test.logSuccess("name_of_site_you_are_testing_here", "login")
+            write_log.logSuccess("name_of_site_you_are_testing_here", "login")
             print("passed %s\n" % (siteName))#(siteName,browser.name.capitalize()))
 
         except Exception as e:
@@ -251,11 +260,11 @@ class login_test(unittest.TestCase):
         finally:
             browser.close()
 
+            
     # Generic login test to a site requiring USERNAME and BIRTHDAY credentials
-    def birthday_login_test(self,siteName,usernameBox,monthBox,dayBox,yearBox,loginButtonName):
+    def birthday_login_test(self,text,siteName,usernameBox,monthBox,dayBox,yearBox,logoutButton):
         assert(self.browser is not None)
         assert(self.bdsites is not None)
-        text = "Account Options"
         browser = self.browser
         
         # Make sure the site credentials exist in your LastPass
@@ -276,45 +285,43 @@ class login_test(unittest.TestCase):
         # Open drivers after ensuring status code was acceptable.
         try:
             browser.get(site['dev_url'])
-            # Call function from log-test.py file to write message to log file
-            log-test.logStart("name_of_site_you_are_testing_here")
+            write_log.logStart("name_of_site_you_are_testing_here")
             print("Testing " + site['dev_url'])
-
-            self.username = site['id']
-            self.mm = site['mm']
-            self.dd = site['dd']
-            self.yr = site['yr']
 
             username_txt = browser.find_element_by_name(usernameBox)
             password_mm = browser.find_element_by_id(monthBox)
             password_dd = browser.find_element_by_id(dayBox)
             password_yr = browser.find_element_by_id(yearBox)
-            submit_txt = browser.find_element_by_name(loginButtonName)
+
             assert(username_txt is not None)
             assert(password_mm is not None)
             assert(password_dd is not None)
             assert(password_yr is not None)
-            assert(submit_txt is not None)
 
             username_txt.clear()
             password_mm.clear()
             password_dd.clear()
             password_yr.clear()
-            username_txt.send_keys(self.username)
-            password_mm.send_keys(self.mm)
-            password_dd.send_keys(self.dd)
-            password_yr.send_keys(self.yr)
-            submit_txt.click()
+            username_txt.send_keys(site['id'])
+            password_mm.send_keys(site['mm'])
+            password_dd.send_keys(site['dd'])
+            password_yr.send_keys(site['yr'])
+            password_yr.send_keys(Keys.ENTER)
 
+            browser.implicitly_wait(30)
+            
             if text not in browser.page_source:
                 print("ERROR: " + text + " not in " + siteName + " source after successful login\n")
             assert(text in browser.page_source)
 
-            #logout_txt = browser.find_element_by_name('logout') # TODO parametrize logout button name
-            #assert(logout_txt is not None)
-            #logout_txt.click()
+            logout_txt = browser.find_element_by_name(logoutButton)
+            if not logout_txt:
+                print(siteName + " has no logout button")
+            else:
+                assert(logout_txt is not None)
+                logout_txt.click()
             
-            log-test.logSuccess("name_of_site_you_are_testing_here", "login")
+            write_log.logSuccess("name_of_site_you_are_testing_here", "login")
             print("passed %s\n" % (siteName))#(siteName,browser.name.capitalize()))
 
         except Exception as e:
@@ -324,7 +331,7 @@ class login_test(unittest.TestCase):
             browser.close()
 
 
-            
+# Begin the test            
 if __name__ == "__main__":
     username = ""
     while "@ucsc.edu" not in username:
